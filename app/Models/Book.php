581dfd75b92a5ffe210af6a1ae5772667aa993f2
esperanza <esperanzatembo@booknowzambia.com>
2024-06-21 18:53:46 +0200

    added fillable attributes

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -1,5 +1,15 @@
 <?php
 
+// namespace App\Models;
+
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Model;
+
+// class Book extends Model
+// {
+//     use HasFactory;
+// }
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,4 +18,7 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'author', 'isbn'];
 }
+
